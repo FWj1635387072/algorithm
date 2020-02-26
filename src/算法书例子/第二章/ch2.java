4c53fdd1c20faef7d0c2cb154ad5fc2d55316fdb
FWj1635387072 <1635387072@qq.com>
2020-02-26 11:24:27 +0800

    蓝桥杯竞赛前的准备

--- src/算法书例子/第二章/ch2.java
+++ src/算法书例子/第二章/ch2.java
@@ -4,9 +4,17 @@ import org.junit.Test;
 import org.omg.CORBA.Any;
 import 算法书例子.第一章.Util;
 
-public class ch2 {
-    //最大和序列
+import java.math.BigInteger;
 
+public class ch2 {
+    //最大子序列和
+
+    /**
+     * Get max subSum1
+     *
+     * @param a
+     * @return
+     */
     static int maxSubSum1(int[] a) {
         int maxSum = 0;
         for (int i = 0; i < a.length; i++) {
@@ -21,6 +29,12 @@ public class ch2 {
         return maxSum;
     }
 
+    /**
+     * Get max subSum2
+     *
+     * @param a
+     * @return
+     */
     static int maxSubSum2(int[] a) {
         int maxSum = 0;
         for (int i = 0; i < a.length; i++) {
@@ -35,13 +49,20 @@ public class ch2 {
         return maxSum;
     }
 
+    /**
+     * Get max subSum3 part1
+     *
+     * @param a
+     * @param left
+     * @param right
+     * @return
+     */
     static int maxSumRec(int[] a, int left, int right) {
         if (left == right)
             if (a[left] > 0)
                 return a[left];
             else
                 return 0;
-
 
         int center = (left + right) / 2;
         int maxLeftSum = maxSumRec(a, left, center);
@@ -64,6 +85,14 @@ public class ch2 {
         return max3(maxLeftSum, maxRightBorderSum, maxLeftBorderSum + maxRightBorderSum);
     }
 
+    /**
+     * Get the Max num
+     *
+     * @param a
+     * @param b
+     * @param c
+     * @return
+     */
     static int max3(int a, int b, int c) {
         if (a > b && a > c)
             return a;
@@ -73,10 +102,22 @@ public class ch2 {
             return c;
     }
 
+    /**
+     * Get max subSum3
+     *
+     * @param a
+     * @return
+     */
     static int maxSubSum3(int[] a) {
         return maxSumRec(a, 0, a.length - 1);
     }
 
+    /**
+     * Get max subSum4
+     *
+     * @param a
+     * @return
+     */
     static int maxSubSum4(int[] a) {
         int thisSum = 0, maxSum = 0;
         for (int i = 0; i < a.length; i++) {
@@ -100,19 +141,82 @@ public class ch2 {
     }
 
 
-    public  static <Anytype extends Comparable<? super Anytype>>
-    int binarySearch(Anytype[] a , Anytype x){
+    /**
+     * binary Search
+     *
+     * @param a
+     * @param x
+     * @param <Anytype>
+     * @return
+     */
+    public static <Anytype extends Comparable<? super Anytype>>
+    int binarySearch(Anytype[] a, Anytype x) {
         int low = 0, high = a.length - 1;
-        while(low <= high){
+        while (low <= high) {
             int mid = (low + high) / 2;
-            if(a[mid].compareTo(x) < 0)
+            if (a[mid].compareTo(x) < 0)
                 low = mid + 1;
-            else if(a[mid].compareTo(x) > 0)
-                high = mid -1;
+            else if (a[mid].compareTo(x) > 0)
+                high = mid - 1;
             else
                 return mid;
         }
         return -1;
     }
 
+
+    /**
+     * gcd
+     * //最大共因数
+     *
+     * @param m
+     * @param n
+     * @return
+     */
+    public static long gcd(long m, long n) {
+        while (n != 0) {
+            long rem = m % n;
+            m = n;
+            n = rem;
+        }
+        return m;
+    }
+
+    /**
+     *  get  x ** n
+     * @param x
+     * @param n
+     * @return
+     */
+    public static BigInteger pow(BigInteger x, BigInteger n) {
+        if (n.equals(0))
+            return new BigInteger("1");
+        if (n.equals(1))
+            return x;
+        if (x.mod(new BigInteger("2")).equals(new BigInteger("0")))
+            return pow(x.multiply(x), n.divide(new BigInteger("2")));
+        else
+            return pow(x.multiply(x), n.divide(new BigInteger("2")).multiply(x));
+    }
+
+    /**
+     * 使用递归进行幂运算(long类型)，其运算范围小于使用math函数，但速度快
+     *  使用BigInteger更好更快
+     * res:
+     * 1.0E20
+     * 1ms
+     * 10000000000000000
+     * 0ms
+     */
+    @Test
+    public void test22(){
+        long l = System.currentTimeMillis();
+        double d =  Math.pow(100,10);
+        System.out.println(d);
+        Util.duration(l);
+
+        l = System.currentTimeMillis();
+        System.out.println(new BigInteger("100").pow(10));
+        Util.duration(l);
+    }
 }
--- src/练习的例子/practice_3.java
+++ src/练习的例子/practice_3.java
@@ -1,5 +1,8 @@
 package 练习的例子;
 
+import org.junit.Test;
+import 算法书例子.第一章.Util;
+
 public class practice_3 {
     public static void main(String[] args) {
         int[] arr = {78, 56, 34, 43, 4, 1, 15, 2, 23};
@@ -52,5 +55,128 @@ public class practice_3 {
         rightOrder(arr, index * 2 + 2);
         System.out.println(arr[index]);
     }
+
+
+    /**
+     * MinHeap 小顶堆
+     *
+     * @param A
+     */
+    static void MinHeap(int[] A) {
+        for (int i = A.length / 2 - 1; i >= 0; i--) {
+            MinHeapFixDown(A, i, A.length);
+        }
+
+    }
+
+
+    static void MinHeapFixDown(int[] A, int i, int n) {
+        //找到左右孩子
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        //左孩子越界，i就是叶子节点
+        if (left >= n) {
+            return;
+        }
+        int min = left;
+        if (right >= n) {
+            min = left;
+        } else {
+            if (A[right] < A[left]) {
+                min = right;
+            }
+        }
+
+        //如果A[i]比两个孩子都要小，不用调整
+        if (A[i] <= A[min]) {
+            return;
+        } else {                  //否则，找到两个孩子中较小的，和i交换
+            int temp = A[i];
+            A[i] = A[min];
+            A[min] = temp;
+        }
+        //小孩子那个位置的值发生了变化，i变更为小孩子那个位置，递归调整
+        MinHeapFixDown(A, min, n);
+    }
+
+    static void sort(int[] a) {
+        MinHeap(a);
+        for (int i = a.length - 1; i >= 0; i--) {
+            swap(a, 0, i);
+            MinHeapFixDown(a, 0, i - 1);
+        }
+    }
+
+    @Test
+    public void test1() {
+       /* int[] a = {4, 2, 6, 1, 3, 5, 7};
+        MinHeap(a);
+        String s = Util.printArr(a);
+        System.out.println(s);
+        a = new int[]{4, 2, 6, 1, 3, 5, 7};
+        sort(a);
+        s = Util.printArr(a);
+        System.out.println(s);*/
+        int[] a = {4, 2, 6, 1, 3, 5, 7};
+        maxHeap(a);
+        String s = Util.printArr(a);
+        System.out.println(s);
+        a = new int[]{4, 2, 6, 1, 3, 5, 7};
+        sort1(a);
+        s = Util.printArr(a);
+        System.out.println(s);
+
+    }
+
+    /**
+     * swap A[a] with A[b]
+     *
+     * @param arr
+     * @param a
+     * @param b
+     */
+    static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+
+    static void maxHeap(int[] a) {
+        for (int i = a.length / 2 - 1; i >= 0; i--) {
+            maxHeapFixDown(a, i, a.length);
+        }
+    }
+
+    static void maxHeapFixDown(int[] A, int i, int n) {
+
+        //找到左右子节点
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        //确定是否有子节点,确定最大值
+        int max = left;
+        if (left >= n)
+            return;
+        if (right >= n) {
+            max = left;
+        } else {
+            if (A[right] > A[left]) {
+                max = right;
+            }
+        }
+        //最大值上浮
+        swap(A, i, max);
+        //递归向下对子树进行上浮
+        maxHeapFixDown(A, max, n);
+    }
+
+    static void sort1(int[] A) {
+        maxHeap(A);
+        for (int i = A.length - 1; i >= 0; i--) {
+            swap(A, 0, i);
+            maxHeapFixDown(A, 0, i - 1);
+        }
+    }
+
 
 }
