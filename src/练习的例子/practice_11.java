1ac8e06cfc02e037ed588e540d6ae8d3458119a8
FWj1635387072 <1635387072@qq.com>
2020-04-13 17:41:14 +0800

    蓝桥杯竞赛前的准备

--- src/练习的例子/practice_11.java
+++ src/练习的例子/practice_11.java
@@ -1,14 +1,70 @@
 package 练习的例子;
 
-import java.util.Set;
+import com.sun.scenario.animation.AbstractMasterTimer;
+import org.junit.Test;
+
+import java.util.*;
 
 public class practice_11 {
-    //非空子集---二进制
+    //子集生成
 
 
-    private Set<Set<Integer>> getSubsets3Core(int[] A, int n, int cur) {
+    static Set<Set<Integer>> getSubsets3Core(int[] A, int n, int cur) {
+        Set<Set<Integer>> newSet = new HashSet<>();
+        if (cur == 0) { //处理第一个元素
+            Set<Integer> nil = new HashSet<>();//空集
+            Set<Integer> first = new HashSet<>();//包含第一个元素的集合
+            first.add(A[0]);
+            newSet.add(nil);
+            newSet.add(first);
+            return newSet;
+        }
+
         Set<Set<Integer>> oldSet = getSubsets3Core(A, n, cur - 1);
-        return null;
+
+        for (Set<Integer> set : oldSet) {
+            //对于每个子集，cur这个元素可以加进去，也可以不加进去。
+            newSet.add(set);//保留原样
+            Set<Integer> clone = (Set<Integer>) ((HashSet) set).clone();
+            clone.add(A[cur]);
+            newSet.add(clone);//添加当前元素
+        }
+        return newSet;
+    }
+
+    static Set<Set<Integer>> getSubsets3Core(int[] A, int n) {
+        return getSubsets3Core(A, n, n - 1);
+    }
+
+    @Test
+    public void test1() {
+        int[] A = {1, 2, 3};
+        Set<Set<Integer>> ne = getSubsets3Core(A, A.length);
+        System.out.println(ne);
+    }
+
+    //二进制解法
+    public ArrayList<ArrayList<Integer>> getSubSets(int[] A, int n) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+
+        int num = (int) Math.pow(2, n) - 1;
+        for (int i = num; i > 0; i--) {
+            ArrayList<Integer> s = new ArrayList<>();
+            for (int j = n - 1; j >= 0; j--) {
+                if (((i >> j) & 1) == 1) {
+                    s.add(A[j]);
+                }
+            }
+            res.add(s);
+        }
+        return res;
+    }
+
+    @Test
+    public void test2() {
+        int[] A = {1, 2, 3};
+        ArrayList<ArrayList<Integer>> subSets = getSubSets(A, A.length);
+        System.out.println(subSets);
     }
 
 }
---
+++ src/练习的例子/practice_12.java
@@ -0,0 +1,9 @@
+package 练习的例子;
+
+public class practice_12 {
+    //全排列
+
+
+
+
+}
