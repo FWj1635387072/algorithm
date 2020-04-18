3bebc67a91757c1f048f9077053836cb72e44cd1
FWj1635387072 <1635387072@qq.com>
2020-04-18 18:52:15 +0800

    蓝桥杯竞赛前的准备

---
+++ src/练习的例子/practice_13_1_DFS练习.java
@@ -0,0 +1,44 @@
+package 练习的例子;
+
+import org.junit.Test;
+
+public class practice_13_1_DFS练习 {
+    //八皇后问题
+
+    int max = 4;
+    int[] arr = new int[4];
+
+    boolean judge(int n) {
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == arr[n] || Math.abs(n - i) == Math.abs(arr[n] - arr[i]))
+                return false;
+        }
+        return true;
+    }
+
+    void check(int n) {
+        if (n == max) {
+            print();
+            return;
+        }
+        for (int i = 0; i < max; i++) {
+            arr[n] = i;
+            if (judge(n)) {
+                check(n + 1);
+            }
+        }
+    }
+
+
+    void print() {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + 1 + " ");
+        }
+        System.out.println();
+    }
+    @Test
+    public void test_八皇后(){
+        check(0);
+    }
+
+}
